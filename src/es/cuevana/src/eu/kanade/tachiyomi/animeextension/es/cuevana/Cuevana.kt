0c621218b3efbbcf4873a62da3213092b87b9d78
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-01-26 01:15:59 +0700

    domain cuevana.bi

--- src/es/cuevana/src/eu/kanade/tachiyomi/animeextension/es/cuevana/Cuevana.kt
+++ src/es/cuevana/src/eu/kanade/tachiyomi/animeextension/es/cuevana/Cuevana.kt
@@ -33,7 +33,7 @@ import okhttp3.Response
 class Cuevana() : ConfigurableAnimeSource, AnimeHttpSource() {
     override val name = "Cuevana"
 
-    override val baseUrl = "https://wv3.cuevana3.eu"
+    override val baseUrl = "https://cuevana.bi"
 
     override val lang = "es"
 
