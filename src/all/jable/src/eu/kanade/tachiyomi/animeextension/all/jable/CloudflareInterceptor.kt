281d0cebb521b28a14214d8bbcb1c14684519629
supermiee <51869118+supermiee@users.noreply.github.com>
2026-06-20 16:38:46 +0800

    fix: remove unused imports from CloudflareInterceptor

--- src/all/jable/src/eu/kanade/tachiyomi/animeextension/all/jable/CloudflareInterceptor.kt
+++ src/all/jable/src/eu/kanade/tachiyomi/animeextension/all/jable/CloudflareInterceptor.kt
@@ -1,7 +1,5 @@
 package eu.kanade.tachiyomi.animeextension.all.jable
 
-import okhttp3.Cookie
-import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
